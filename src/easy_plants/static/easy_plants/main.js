90b5f926aa23add55d41279fc5cb5258ec281dea
Kai Diefenbach <kai.diefenbach@iqpp.de>
2025-05-06 11:31:46 +0200

    Added Flatpickr support for datetime fields with time picker

--- src/easy_plants/static/easy_plants/main.js
+++ src/easy_plants/static/easy_plants/main.js
@@ -29,8 +29,18 @@ function stopRotation() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Initialisiere Flatpickr für Datumsfelder
     flatpickr(".dateinput, input[type='date']", {
         dateFormat: "d.m.Y",
+        locale: "de",
+        allowInput: true
+    });
+
+    // Initialisiere Flatpickr für Datum/Zeit-Felder
+    flatpickr(".datetimeinput", {
+        enableTime: true,
+        dateFormat: "d.m.Y H:i",
+        time_24hr: true,
         locale: "de",
         allowInput: true
     });
